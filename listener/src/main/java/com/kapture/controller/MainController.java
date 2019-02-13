1aefb7f013e0b6d372a0f876bb5ab26f6da89777
vickyrathod <vickyrathod055@gmail.com>
2019-02-13 15:19:48 +0530

    Update MainController.java
    updated

--- listener/src/main/java/com/kapture/controller/MainController.java
+++ listener/src/main/java/com/kapture/controller/MainController.java
@@ -13,10 +13,27 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 @RequestMapping("/")
 public class MainController {
 	
+	
+	private int timestamp;
+
+	@GetMapping("gettime")
+	public ResponseEntity<Integer> getTime() {
+		return ResponseEntity.status(HttpStatus.OK).body(getTimestamp());
+
+	}
+
 	@PostMapping("postdata")
-	public ResponseEntity<Object> listenData(@RequestBody String str){
+	public ResponseEntity<Object> listenData(@RequestBody String str) {
 		System.out.println(str);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	public int getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(int timestamp) {
+		this.timestamp = timestamp;
 	}
 
 }
